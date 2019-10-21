ba0f2a3a467df5c8f33764a988900b857f8a8dda
jlawless <johnjacklawless@gmail.com>
2019-10-21 15:33:24 +0000

    Solved.

--- products/handler.py
+++ products/handler.py
@@ -1,6 +1,7 @@
 import simplejson as json
 import boto3
 import sys
+import requests
 sys.path.insert(0, "vendored")
 def geturi(event, context):
   uri_path = event["path"]
@@ -8,6 +9,11 @@ def geturi(event, context):
   dynamodb = boto3.resource('dynamodb')
   table = dynamodb.Table('product')
   item = table.get_item(Key={'id': str(path_dict[2])})
-  body = {}
-  response = {"statusCode": 200, "body": json.dumps(item['Item'])}
+  item = item['Item']
+  item_price = item['price']
+  base_uri = 'https://redsky.target.com/v2/pdp/tcin/'
+  item_uri = base_uri + str(path_dict[2])
+  r = requests.get(item_uri)
+  response = {"statusCode": 200, "body": json.dumps({'Redsky':r.text, 'Price_fromDynamo': item_price})}
   return response
+
